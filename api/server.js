4a15442727e4b68771595a46e6d75f4a045a9527
Arkadiusz Rak <arek93r@gmail.com>
2025-01-19 09:59:46 +0100

    server + index run

--- api/server.js
+++ api/server.js
@@ -1,1 +1,11 @@
 // build your server here and require it from index.js
+const express = require("express");
+
+const server = express();
+server.use(express.json());
+
+server.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
+module.exports = server;
